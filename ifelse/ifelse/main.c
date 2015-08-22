79e32113e4b480c21e67fc0a77e0d7799ec7dbf7
pierangelo1982 <pierangelo1982@gmail.com>
2015-08-22 17:48:29 +0200

    ifelse

---
+++ ifelse/ifelse/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  ifelse
+//
+//  Created by pierangelo on 22/08/15.
+//  Copyright (c) 2015 pierangelo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int valore;
+    
+    printf("Inserisci un valore numerico\n");
+    scanf("%d", &valore);
+    
+    if (valore >= 60) {
+        printf("sei stato promosso");
+    }
+    else {
+        printf("sei stato bocciato");
+    }
+
+}
