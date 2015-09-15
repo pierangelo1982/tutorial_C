aa58cdaac874dd8b334e407f399e66e1614a3d91
pierangelo1982 <pierangelo1982@gmail.com>
2015-09-16 01:55:44 +0200

    forloop

---
+++ ForLoop/ForLoop/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ForLoop
+//
+//  Created by pierangelo on 16/09/15.
+//  Copyright (c) 2015 pierangelo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
