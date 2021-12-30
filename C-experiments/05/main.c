513fe111a57c1a163cfeb85b3c25e3492c02bf57
pierangelo1982 <pierangelo1982@gmail.com>
2021-12-30 17:30:29 +0100

    fahrenhait celsius converter

--- C-experiments/05/main.c
+++ C-experiments/05/main.c
@@ -14,9 +14,24 @@ void main()
                        adiacenti */
 
     fahr = lower;
+    printf("fahr\tcelsius\n");
     while(fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+
+    lower = 0;      /* valore minimo in gradi Celsius nella tabella delle
+                       temperature  */
+    upper = 300;     /* valore massimoin gradi Celsius   */
+    step =  20;     /* intervallo fra due temperature in gradi Celsius
+                      adiacenti */
+    
+    celsius = lower;
+    printf("\ncelsius\tfahr\n");
+    while(celsius <= upper) {
+        fahr = (celsius * 9.0/5.0) + 32.0;
+        printf("%3.0f\t%3.0f\n", celsius, fahr);
+        celsius = celsius + step;
+  } 
 }
